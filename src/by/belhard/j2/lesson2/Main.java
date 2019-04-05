741f8723c510fdfeb5e61ee67bdbfb3e567a27be
wydora <nikolay.vitko@gmail.com>
2019-04-05 21:30:21 +0300

    Konec pari

--- src/by/belhard/j2/lesson2/Main.java
+++ src/by/belhard/j2/lesson2/Main.java
@@ -1,9 +1,36 @@
 package by.belhard.j2.lesson2;
 
 public class Main {
- int x=15;
+
 
     public static void main(String[] args) {
 
+        int x = 22;
+
+       /* /if (x == 20) {
+            x--;
+        } else if (x == 21){
+            x++;
+        } else{
+            x /= 2;
+        }*/
+
+
+       // System.out.println(x);
+       // System.out.println(( x >= 20 ? x/2 : -x));
+        /*while (x > 15) {
+
+            System.out.println(x);
+
+            x--;
+        }*/
+        /*do {
+            System.out.println(x);
+            x--;
+        } while (x > 20);
+        }*/
+        for (int i = 0; i <= 3; i++){
+            System.out.println(i);}
+
+        }
     }
-}
