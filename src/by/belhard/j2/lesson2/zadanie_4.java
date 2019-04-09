c11ae2ef3c4e72472dc0514fb05af1f0805a68ea
wydora <nikolay.vitko@gmail.com>
2019-04-09 13:07:11 -0700

    zadanie_3

---
+++ src/by/belhard/j2/lesson2/zadanie_4.java
@@ -0,0 +1,56 @@
+package by.belhard.j2.lesson2;
+
+public class zadanie_4 {
+    public static void main(String[] args) {
+        /*int a, b, c, d;
+        a = 0;
+        b = 2;
+        d = 3;
+
+        do {
+
+            c = b*a;
+            System.out.print(c);
+            System.out.print(" ");
+            a++;
+
+        }
+        while (a <= 10);
+    }*/
+        for (int j = 0; j <= 20; j += 2) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 30; j += 3) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 40; j += 4) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 50; j += 5) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 60; j += 6) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 70; j += 7) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 80; j += 8) {
+            System.out.print(j + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j <= 90; j += 9) {
+            System.out.print(j + " ");
+        }
+
+
+    }
+}
+
+
