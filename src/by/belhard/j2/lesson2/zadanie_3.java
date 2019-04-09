bfa9372075809dfc4945a2bdd4fc4b172fbaf63c
wydora <nikolay.vitko@gmail.com>
2019-04-09 12:45:42 -0700

    zadanie_3

---
+++ src/by/belhard/j2/lesson2/zadanie_3.java
@@ -0,0 +1,22 @@
+package by.belhard.j2.lesson2;
+
+public class zadanie_3 {
+    public static void main(String[] args) {
+        int b, c;
+
+
+        c = 3;
+        b = 0;
+
+        do {
+            System.out.println(b);
+            b += c;
+
+        }
+           while (b <= 100);
+
+           
+        }
+    }
+
+
